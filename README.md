540c06d07e4220485ecd97b0f4459a8b1c1e30c3
Siddharthm10 <siddharth.mehtaid@gmail.com>
2022-05-16 14:17:21 +0530

    Readme and GUI aded for ease in Installation

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
 # ResultScrapper
+- This is currently only optimized for chrome.
 - This gives the updated cgpi of all the students present.
-- When executed all the data will be saved to a csv file and then can be used easily.
-- Currently it's designed to scrape 6th semester results.
+- When executed all the data will be saved to a excel file and then can be used easily.
+- Currently it's designed to scrape any semester's results.
 
-### Changes to be made:
-- Change the executable path for the browser
-- Change the following details.
-  -  `minRoll`
-  -  `maxRoll` 
-  -  `rollNosNotPresent` (Only for faster results) (It's okay if you dont edit it)
-- Install dependencies with: `pip install -r requirements.txt`
+### Pre-Requisites:
+- Download latest chrome-driver from [here](https://chromedriver.chromium.org/downloads) and place it in the chromedriver folder.
+- Make a csv file with all the roll number in a column named `RollNo`
+- Change the executable path for the browser - [Search](https://www.google.com/search?q=How+to+find+executable+file+of+an+application&btnK=Google+Search&sxsrf=AOaemvJgYJoJHPiQ9ydHP0jJZzXpfa8D1Q%3A1632723732491&source=hp&ei=FGNRYZfbG63C5OUPwJuJOA&iflsig=ALs-wAMAAAAAYVFxJG-sFSzD5s1x6Wx6MHQ2uczFYfSp&oq=min+number+of+boys+or+girls+together+c%2B%2B&gs_lcp=Cgdnd3Mtd2l6EANQ1EFY1EFgmUpoAHAAeACAAa4BiAGuAZIBAzAuMZgBAKABAqABAQ&sclient=gws-wiz&ved=0ahUKEwiXtdvwwZ7zAhUtIbkGHcBNAgcQ4dUDCAg&uact=5) :P
+- Install dependencies with: 
+    `pip install -r requirements.txt`
 
 ### Execute the script
 To run the script : `python main.py`
-
+  
 ##### All issues/Contributions are welcomed :)
--- main.py
+++ main.py
@@ -1,42 +1,93 @@
+import time
+from numpy import roll
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
+from tkinter import *
 
-URL = "http://59.144.74.15/scheme18/studentresult/index.asp"
+
+URL = "http://14.139.56.19/scheme18/studentresult/index.asp"
 PATH = "./chromedriver"
 options = Options()
 options.binary_location = "/usr/bin/brave-browser-stable" ### Place the executable file of your browser
-driver = webdriver.Chrome(options = options, executable_path= PATH)
 
+
+output  = [] 
+f_name = ""
+semester = 1
+branch = ""
+
+def takeInps():
+    global sem, semester
+    global rolls_fname, f_name
+    global br, branch
+    semester = int(sem.get())
+    f_name = rolls_fname.get()
+    branch = br.get()
+    root.destroy()
+
+
+#Take Input
+root = Tk()
+root.title("Result Scrapper NITH")
+root.geometry("200x200")
+
+# Semester block:
+l = Label(root, text="Semester Count:")
+l.pack()
+sem = Entry(root)
+sem.pack()
+sem.focus_set()
+
+# Filename block
+l = Label(root, text="Roll No filename")
+l.pack()
+rolls_fname = Entry(root)
+rolls_fname.pack()
+
+# branch
+l = Label(root, text="Branch")
+l.pack()
+br = Entry(root)
+br.pack()
+
+#Submit Button
+sbmt = Button(root, text="Submit",command=takeInps)
+sbmt.pack(side="bottom")
+
+# start app
+root.mainloop()
+
+table_no = semester*2+2
+rollNos =  pd.read_csv(f_name)["RollNo"].to_numpy()
+
+
+
+driver = webdriver.Chrome(options = options, executable_path= PATH)
 driver.get(URL)
 
-############Config##########################
-rollNosNotPresent = []
-minRoll = 184001
-maxRoll = 184095
-#############################################
-
-rollNos = [x for x in range(minRoll,maxRoll+1) if x not in rollNosNotPresent]
-output  = [] 
 
 for roll in rollNos:
+    roll = int(roll)
+    
     try:
-        rollNoInput = WebDriverWait(driver, 5).until(
+        rollNoInput = WebDriverWait(driver, 7).until(
                     EC.presence_of_element_located((By.XPATH, '/html/body/div/div/div/div[2]/div[2]/form/p[4]/input'))
                 )
         rollNoInput.clear()
         rollNoInput.send_keys(roll)
 
-        submit = WebDriverWait(driver, 5).until(
+        submit = WebDriverWait(driver, 7).until(
                 EC.presence_of_element_located((By.XPATH, '/html/body/div/div/div/div[2]/div[2]/form/p[6]/input[2]'))
             )
         submit.click()
 
-        cgpi = WebDriverWait(driver, 5).until(
-                EC.presence_of_element_located((By.XPATH, '/html/body/div/div[8]/div[2]/div/div/table[2]/thead/tr[2]/td[4]'))
+        cgpi_xpath = f'/html/body/div/table[{table_no}]/tbody/tr/td[4]/p[2]'
+        cgpi = WebDriverWait(driver, 7).until(
+                EC.presence_of_element_located((By.XPATH, cgpi_xpath))
             )
 
 
@@ -47,13 +98,14 @@ for roll in rollNos:
 
 
     except:
-        print(f"data relevant to {roll} not found!")
-        driver.get(URL)
+        output.append([roll, "NF"])
+        print(f"[ERROR LOG] - Data relevant to {roll} not found! Request Failed!!!")
+    
     
 
 data = pd.DataFrame(data=output, columns=["RollNo", "Cgpi"])
 print(data)
-data.to_csv("./Updatedcgpi.csv")
+data.to_csv(f'{semester}_result_{branch}.csv')
 
 driver.quit()
 
